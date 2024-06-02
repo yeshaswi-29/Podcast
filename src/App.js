484e6fdc492d38d16e578acccc831f3c3f2cd11a
Samikshakhapare <samikshakhapare225@gmail.com>
2024-06-02 20:28:30 +0530

    Sidebar and Navbar created

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import styled, { ThemeProvider } from "styled-components";
+import {lightTheme, darkTheme} from ".utils/Theme"
+
+const container = styled.dev`
+background : ${({theme})=> theme.bg};
+width: 100vh;
+height: 100vh;
+`;
 
 function App() {
+// ________________________________For Theme___________________________________
+  //hooks
+
+  const [darkMode, setDarkMode] = useState(true);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <container>Podcast</container>
+    </ThemeProvider>
   );
 }
 
